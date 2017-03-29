0bd1be1d15106229e75467ff81215cd92973f853
Len Chen <wheels.lj@gmail.com>
2017-03-29 22:13:06 +0800

    Initial Commit

---
+++ ReduxInObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReduxInObjC
+//
+//  Created by 陳冠綸 on 2017/3/29.
+//  Copyright © 2017年 陳冠綸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReduxInObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReduxInObjC
+//
+//  Created by 陳冠綸 on 2017/3/29.
+//  Copyright © 2017年 陳冠綸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
